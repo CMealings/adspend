0e997fd02399086ed02ffeee6b24bb4114703326
Charlie Mealings <cmealings@gmail.com>
2018-01-25 12:53:55 +0000

    load data function drafted

--- scripts/loadData.R
+++ scripts/loadData.R
@@ -1,4 +1,4 @@
-reqPackages(c("xlsx", "abind"))
+reqPackages(c("xlsx", "abind", "readxl"))
 
 folder <- "E:/TYPESET/International Data/Adspend database/Database (NEW FORMAT)/"
 
@@ -10,37 +10,41 @@ datasets <- datasets[-grep("^~\\$", datasets)]
 
 df <- NULL
 mf <- NULL
+sheet.names <- NULL
+
+start <- Sys.time()
 
 for (wb in datasets) {
   
   path <- paste0(folder, wb)
-  file <- loadWorkbook(path)
   
-  sheets <- names(getSheets(file))
-  sheets <- sheets[!sheets %in% c("Sheet2", "Sheet3", "Countries", "Asia Pacific Counts", "Africa Counts", "Latin America Counts", "MiddleEast Counts", "Template")]
+  sheets <- excel_sheets(path)
+  sheets <- sheets[!sheets %in% c("Sheet2", "Sheet3", "Countries", "Asia Pacific Counts", "Africa Counts", "Latin America Counts", "Europe Counts", "MiddleEast Counts", "Template")]
   
   for (sht in sheets) {
     
-    # data <- read.xlsx(path, sht, rowIndex = 23:60, colIndex = 1:55, keepFormulas = FALSE, header = FALSE, row.names = 1)
-    data <- readWorksheet(file, sht, startRow = 1, endRow = 60, startCol = 1, endCol = 55, useCachedValues = TRUE, header = FALSE, stringsAsFactors = FALSE)
-    browser()
-    # meta <- read.xlsx(path, sht, rowIndex = 14:21, colIndex = 1:55, header = FALSE, stringsAsFactors = FALSE, row.names = 1)
+    data <- read_excel(path, sht, range = "A1:CF60", col_names = FALSE)
     
-    countryMeta <- read.xlsx(path, sht, rowIndex = 1:3, colIndex = 1, header = FALSE, stringsAsFactors = FALSE)
-    setName <- countryMeta[1, 1]
-    dateUpdated <- countryMeta[2, 1]
-    regionCode <- countryMeta[3, 1]
+    setName <- as.character(data[1, 2])
+    dateUpdated <- as.numeric(data[2, 2][[1]])
+    regionCode <- as.character(data[3, 2])
     
-    meta <- meta[14:21, ]
-    meta[5:7, ] <- apply(meta[5:7, ], 1:2, as.numeric)
+    meta <- as.data.frame(data[14:21, -1])
+    rownames(meta) <- data[14:21, 1][[1]]
+    meta[6:8, ] <- apply(meta[6:8, ], 1:2, as.numeric)
     
-    meta <- list("setName" = setName, "dateUpdated" = dateUpdated, "regionCode" = regionCode, "meta" = meta)
+    meta <- list("setName" = setName, "dateUpdated" = dateUpdated, "regionCode" = regionCode, "data" = meta)
+    data <- as.data.frame(data[23:60, -1], row.names = data[23:60, 1][[1]], colClasses = rep("numeric", dim(data)[2] - 1))
     
-    colnames(data) <- meta["SeriesCode", ]
+    colnames(data) <- meta$data["SeriesName", ]
     
-    mf <- abind(mf, meta, along = 3)
+    mf <- list(mf, list(meta))
     df <- abind(df, data, along = 3)
   }
+  
+  sheet.names <- append(sheet.names, sheets)
 }
 
+dimnames(df)[[3]] <- sheet.names
+cat(Sys.time() - start)
 
